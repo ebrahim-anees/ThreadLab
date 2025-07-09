99ffca1f6d795791a27f5b9bbd8ace56be4d9f31
Ebrahim-Anees <ebrahimaneesabuel7@gmail.com>
2025-07-09 17:38:32 +0300

    fix#2

--- server/server.js
+++ server/server.js
@@ -47,4 +47,5 @@ app.use('/api/shop/review', productReviewRouter);
 app.use('/api/common/features', commonFeaturesRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
+// app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
+export default app;
